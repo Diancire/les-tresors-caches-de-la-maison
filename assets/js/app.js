b655b4b41d488fc96178985ccbc273ac4be2cc22
Dianciré Diallo <diaancir@gmail.com>
2023-09-09 21:33:34 +0200

    tablet and mobile responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,13 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/app.scss';
+
+const toggle_btn = document.querySelector('.navbar_icon_burger')
+const toggle_btn_icon = document.querySelector('.navbar_icon_burger i')
+const dropdown_menu = document.querySelector('.navbar_dropdown_menu')
+
+toggle_btn.onclick = function () {
+    dropdown_menu.classList.toggle('is-open');
+    const isOpen = dropdown_menu.classList.contains('is-open')
+    toggle_btn_icon.classList = isOpen ? "fa fa-xmark" : "fa fa-bars"
+}
