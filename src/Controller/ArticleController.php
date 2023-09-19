fe3fa718a32f0500bc3c6d0de4219d0f08cc5428
Dianciré Diallo <diaancir@gmail.com>
2023-09-19 17:04:35 +0200

    feat(search-bar): setting up the search bar

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\SearchType;
+use App\Model\SearchData;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Knp\Component\Pager\PaginatorInterface;
@@ -16,12 +18,28 @@ class ArticleController extends AbstractController
     #[Route('/blog', name: 'app_blog')]
     public function index(ArticleRepository $articleRepository, Request $request, CategoryRepository $categoryRepository): Response
     {
+        $searchData = new SearchData();
+
+        $form = $this->createForm(SearchType::class, $searchData);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $searchData->page = $request->query->getInt('page', 1);
+            $articles = $articleRepository->findBySearch($searchData);
+            $categories = $categoryRepository->findAll();
+            return $this->render('article/index.html.twig', [
+                'articles' => $articles,
+                'form' => $form,             
+                'categories' => $categories, 
+            ]);
+        }
         
         $articles = $articleRepository->findPublished($request->query->getInt('page', 1));
         $categories = $categoryRepository->findAll();
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
-            'categories' => $categories
+            'categories' => $categories, 
+            'form' => $form
         ]);
     }
     #[Route('/blog/{slug}', name: 'app_blog_show')]
