7a005b59d0cbfedc93bb99e95da1f49c95eb0d07
Dianciré Diallo <diaancir@gmail.com>
2023-09-19 21:10:23 +0200

    feat(unique-nickname): modification du pseudo en unique et modification du tableau gestion des users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 #[UniqueEntity(fields: ['email'], message: 'Cet email existe déjà au sein de l\'application')]
+#[UniqueEntity(fields: ['nickname'], message: 'Ce pseudo existe déjà au sein de l\'application')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -33,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $nickname = null;
 
     #[ORM\Column(length: 255)]
